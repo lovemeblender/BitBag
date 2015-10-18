1bd64a2e9cc6653f8c45d1c97333619522cc41bf
Michael Despotopoulos <michaeldespoto@gmail.com>
2015-10-18 19:13:15 +0300

    v1.1 - Major update. Now backed by dynamic array.

--- BitBag.java
+++ BitBag.java
@@ -1,29 +1,51 @@
+package storage;
+
+import java.util.Arrays;
 
 public class BitBag {
 	
-	int bitPattern;
-	private String strBitPattern;
-	
-	public Bit() {
-		bitPattern = 0;
-		strBitPattern = "";	
+	private int[] bitArray; // each slot holds 31 bits
+	private int n; // array size
+		
+	public BitBag() {
+		this(1);
+	}
+
+	public BitBag(int n) {
+		bitArray = new int[n];
+		this.n = n;
 	}	
 	
-	public String decToBin() {
-		StringBuilder sb = new StringBuilder();
-		int bit = bitPattern;
+	// Binary representation of the array
+	private String decToBin() {
 		
-		while(bit > 0) {
-			if(bit % 2 == 0) sb.append("0");
-			else sb.append("1");	
-			bit /= 2;		
+		StringBuilder sb = new StringBuilder();
+		
+		for(int i : bitArray) {
+	
+			int zeros = 0;
+			while(i > 0) {
+				if(i % 2 == 0) sb.append("0");
+				else sb.append("1");	
+				i /= 2;
+				zeros--;
+			}
+			// append zeros
+			zeros += 31;
+			while(zeros > 0) {
+				sb.append("0");
+				zeros--;
+			}
+			sb.append(" ");
 		}
+	
+		sb.setLength(sb.length() - 1); // delete last white space
 
-		return sb.reverse().toString();
+		return sb.toString();
 	}
 
 	public int binToDec(String bin) {
-		StringBuilder sb = new StringBuilder();
+		
 		int num = 0;
 		int pos = 0;
 		
@@ -37,47 +59,56 @@ public class BitBag {
 		
 		return num;
 	}
-	
-	public int getInt() {
-		return bitPattern;	
-	}
 
 	// Sets the nth bit of the pattern
 	public void set(int n) {
-		int mask = (int) Math.pow(2, n);
-		bitPattern = bitPattern | mask;
+		if(n < 0) throw new IndexOutOfBoundsException("index < 0");
+		if(n > (bitArray.length * 31) - 1) resize(n); // Expand array
+		
+		int mask = (int) Math.pow(2, n - (31 * (n/31)));
+		
+		bitArray[ n / 31 ] = bitArray[ n / 31 ] | mask;
 	}
 	
+	private void resize(int n) {
+		// Create a larger array
+		int[] expanded = new int[ (n / 31) + 1];
+		
+		// Dump bitArray into expanded
+		for(int i = 0; i < bitArray.length; i++) {
+			expanded[i] = bitArray[i];
+		}
+		bitArray = expanded;
+		this.n = bitArray.length;
+	}
+
 	// Gets the nth bit of the pattern
 	public int get(int n) {
-		int mask = (int) Math.pow(2, n);
-		return (bitPattern & mask) == 0 ? 0 : 1;	
+		if(n < 0) throw new IndexOutOfBoundsException("index < 0");
+		if(n > (bitArray.length * 31) - 1) throw new IndexOutOfBoundsException("index > size");
+			
+		int mask = (int) Math.pow(2, n - (31 * (n/31)));
+		return (bitArray[ n / 31 ] & mask) == 0 ? 0 : 1;	
 	}
 
 	// Clears the nth bit
 	public void clear(int n) {
-		int mask = ~(int) Math.pow(2, n);
-		bitPattern = bitPattern & mask; 		
+		int mask = ~(int) Math.pow(2, n - (31 * (n/31)));
+		bitArray[ n / 31 ] = bitArray[ n / 31 ] & mask;
 	}
 
+	// Clear all bits in array
 	public void clearAll() {
-		bitPattern = 0;	
+		bitArray = new int[n];	
+	}
+	
+	@Override	
+	public String toString() {
+		return decToBin();		
 	}
 
-	public BitBag and(BitBag b) {
-		return this.bitPattern & b.bitPattern;
-	}
-
-	public static void main(String[] args) {
-		BitBag b = new BitBag();
-		b.set(0);
-		b.set(2);
-		b.set(3);
-			
-		System.out.println(b.decToBin());
-		b.clear(2);
-		System.out.println(b.decToBin());
-		b.clear(3);
-		System.out.println(b.decToBin());
+	// Prints the numerical form of the array
+	public void printArray() {
+		System.out.println(Arrays.toString(bitArray));	
 	}
 }
